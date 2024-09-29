545a6fc90074fa584cca599f8761639ba12c34a4
Eve Delisle <EveP.Delisle@gmail.com>
2024-09-29 16:39:12 -0400

    Movement étoiles hero

--- src/scripts/components/Lottie.js
+++ src/scripts/components/Lottie.js
@@ -1,39 +1,29 @@
-import lottie from 'lottie-web';
-
 export default class Lottie {
   constructor(element) {
     this.element = element;
+    this.isReversed = false;
     this.init();
   }
 
   init() {
     this.loadLottieAnimation();
+    this.AnimationReverse();
   }
 
-  loadLottieAnimation() {
-    const animation = lottie.loadAnimation({
-      container: document.getElementById('lottie-container1'),
-      renderer: 'svg',
-      loop: false,
-      autoplay: true,
-      path: 'https://lottie.host/33c44ba9-4683-4417-a350-a74241bc061e/xQ6UlyDoot.json',
+  loadLottieAnimation() {}
+
+  AnimationReverse() {
+    const player = this.element;
+
+    player.addEventListener('ready', () => {
+      console.log('Lottie player is ready');
+
+      // Now, add the complete event listener
+      player.addEventListener('complete', () => {
+        this.isReversed = !this.isReversed; // Toggle the direction flag
+        player.setDirection(this.isReversed ? -1 : 1); // -1 to reverse, 1 to play forward
+        player.play(); // Restart the animation with the new direction
+      });
     });
-
-    let isReversed = false; // Declare a mutable variable to track animation direction
-
-    // Function to reverse the animation direction
-    const toggleDirection = () => {
-      isReversed = !isReversed; // Toggle reverse state
-      animation.setDirection(isReversed ? -1 : 1); // -1 for reverse, 1 for forward
-      animation.play(); // Play the animation in the new direction
-    };
-
-    // Listen for the complete event to trigger the direction change
-    animation.addEventListener('complete', function () {
-      toggleDirection(); // Reverse direction once the animation completes
-    });
-
-    // Optional: Control speed
-    animation.setSpeed(1); // Change this value to control speed (1 is normal speed)
   }
 }
