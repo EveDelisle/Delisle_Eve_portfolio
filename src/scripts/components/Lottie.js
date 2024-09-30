03e3a8d517b71cc63871b486305c68e1e138a84e
Eve Delisle <EveP.Delisle@gmail.com>
2024-09-29 20:22:02 -0400

    Lottie reverse

--- src/scripts/components/Lottie.js
+++ src/scripts/components/Lottie.js
@@ -1,3 +1,5 @@
+import { LottieInteractivity } from '@lottiefiles/lottie-interactivity';
+
 export default class Lottie {
   constructor(element) {
     this.element = element;
@@ -10,20 +12,37 @@ export default class Lottie {
     this.AnimationReverse();
   }
 
-  loadLottieAnimation() {}
+  loadLottieAnimation() {
+    document.addEventListener('DOMContentLoaded', () => {
+      LottieInteractivity.create({
+        player: myLottieAnimation,
+        mode: 'scroll',
+        actions: [
+          {
+            visibility: [0, 1], // Trigger when the animation is visible within the viewport
+            type: 'seek',
+            frames: [0, 100], // Define the frame range for the animation
+          },
+        ],
+      });
+    });
+  }
 
   AnimationReverse() {
     const player = this.element;
 
-    player.addEventListener('ready', () => {
-      console.log('Lottie player is ready');
+    if ('reverse' in this.element.dataset) {
+      player.addEventListener('ready', () => {
+        console.log('Lottie player is ready');
 
-      // Now, add the complete event listener
-      player.addEventListener('complete', () => {
-        this.isReversed = !this.isReversed; // Toggle the direction flag
-        player.setDirection(this.isReversed ? -1 : 1); // -1 to reverse, 1 to play forward
-        player.play(); // Restart the animation with the new direction
+        // Now, add the complete event listener
+
+        player.addEventListener('complete', () => {
+          this.isReversed = !this.isReversed; // Toggle the direction flag
+          player.setDirection(this.isReversed ? -1 : 1); // -1 to reverse, 1 to play forward
+          player.play(); // Restart the animation with the new direction
+        });
       });
-    });
+    }
   }
 }
