4f5c3e30354770fa587c043b61ddc13a51e5a927
Eve Delisle <EveP.Delisle@gmail.com>
2024-10-02 21:07:31 -0400

    animation image hero

--- src/scripts/ComponentFactory.js
+++ src/scripts/ComponentFactory.js
@@ -1,6 +1,7 @@
 import Carousel from './components/Carousel';
 import Header from './components/Header';
 import Lottie from './components/Lottie';
+import Scrolly from './components/Scrolly';
 
 export default class ComponentFactory {
   constructor() {
@@ -9,6 +10,7 @@ export default class ComponentFactory {
       Carousel,
       Header,
       Lottie,
+      Scrolly,
     };
     this.init();
   }
